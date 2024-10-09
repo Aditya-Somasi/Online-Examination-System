b936203575a22cb80864fe53629ad4f7267fdcd9
SOMASI VENKATA SIVA KAMESWARA ADITYA <adityasomasi@gmail.com>
2024-10-09 18:11:16 +0530

    Add files via upload

---
+++ frontend/build/precache-manifest.fc4f7b10ecce0a43be3ecae5d5bb6f82.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a24e93f2b570d5a7bf23517a60f68da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2766fef84fe9126094f",
+    "url": "/static/css/2.46439c26.chunk.css"
+  },
+  {
+    "revision": "b16fd7f47616742d175f",
+    "url": "/static/css/main.40afb3a9.chunk.css"
+  },
+  {
+    "revision": "d2766fef84fe9126094f",
+    "url": "/static/js/2.e4155a12.chunk.js"
+  },
+  {
+    "revision": "b16fd7f47616742d175f",
+    "url": "/static/js/main.d2ba139f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4401e82d47a16230fd580bcbdb7c5d49",
+    "url": "/static/media/homepage.4401e82d.jpeg"
+  },
+  {
+    "revision": "29212f5ee6f1829c59a8b4735f2e513e",
+    "url": "/static/media/main.29212f5e.jpg"
+  }
+]);
